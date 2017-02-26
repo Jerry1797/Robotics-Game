4d2308499fd24973d025bd60495b50d2b38a517e
Jason <jam92@njit.edu>
2017-02-26 16:27:28 -0500

    GoalSystem v1

---
+++ Assets/Scripts/Grid/Goal.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	public int order;
+	public bool activated;
+    public Color ActivateColor;
+    public Color DeactivateColor;
+    public Material ReferenceMaterial;
+    private Material m_Material;
+
+	// Use this for initialization
+	void Start () {
+		activated = true;
+        gameObject.GetComponent<Renderer>().material = Material.Instantiate(ReferenceMaterial);
+        m_Material = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void Activate()
+	{
+		Debug.Log ("Goal Was Activated");
+        //Set Color
+        m_Material.color = ActivateColor;
+        m_Material.SetColor("_EmissionColor", ActivateColor);
+
+        activated = true;
+
+	}
+	public void Deactivate()
+	{
+		Debug.Log ("Goal Was Deactivated");
+        //Set Color
+        m_Material.color = DeactivateColor;
+        m_Material.SetColor("_EmissionColor", DeactivateColor);
+
+        activated = false;
+	}
+	public void SetIndex(int i)
+	{
+		order = i;
+	}
+	public int GetIndex()
+	{
+		return order;
+	}
+}
---
+++ Assets/Scripts/Grid/GoalManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class GoalManager : MonoBehaviour {
+
+	public List<Transform> Goals;
+	public int CurrentGoalIndex;
+
+	// Use this for initialization
+	void Start () {
+		CurrentGoalIndex = 0;
+        for(int i = 0;i < Goals.Count; i++) { 
+            Goals[i].GetComponent<Goal>().SetIndex (i);
+            Goals[i].GetComponent<Goal>().Activate();
+        }
+	
+	}
+
+	public void DeactivateGoal(int index)
+	{
+		//Check Index
+
+
+		//Activate Goal
+		Goals[index].GetComponent<Goal>().Deactivate();
+		CurrentGoalIndex++;
+	}
+
+	public void Reset()
+	{
+		CurrentGoalIndex = 0;
+		foreach (Transform g in Goals) {
+			g.GetComponent<Goal> ().Activate ();
+		}
+	}
+}
